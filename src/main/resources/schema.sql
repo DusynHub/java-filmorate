7664365ae79102d2c5ad1a408e60f922a8cfd0df
DusynHub <gooddayemail@gmail.com>
2023-03-15 19:31:25 +0300

    Правки в базу данных от Владимира, Сергея и Артёма. Все изменения в файле schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,6 +5,10 @@ drop table if exists LIKES cascade;
 drop table if exists USERS cascade;
 drop table if exists FILM_GENRE cascade;
 drop table if exists FRIENDSHIP cascade;
+drop table if exists REVIEWS cascade;
+drop table if exists FEED_LIST cascade;
+drop table if exists DIRECTOR cascade;
+drop table if exists FILM_DIRECTOR cascade;
 
 create table IF NOT EXISTS GENRE
 (
@@ -112,3 +116,78 @@ create unique index IF NOT EXISTS LIKES_ID_UINDEX
 
 create unique index IF NOT EXISTS USER_ID_UINDEX
     on USERS (ID);
+
+create table IF NOT EXISTS REVIEWS
+(
+    ID           BIGINT not null,
+    FILM_ID      BIGINT not null,
+    USER_ID      BIGINT not null,
+    CONTENT      CHARACTER VARYING(250),
+    "isPositive" BOOLEAN,
+    USEFUL       INTEGER,
+    COLUMN_7     INTEGER,
+    constraint REVIEWS_PK
+        primary key (ID),
+    constraint REVIEWS_FK
+        foreign key (ID) references FILM
+            on update cascade on delete cascade
+);
+
+create unique index REVIEWS_ID_UINDEX
+    on REVIEWS (ID);
+
+create table IF NOT EXISTS event_type
+(
+    event_type ENUM ('LIKE', 'REVIEW', 'FRIEND')
+);
+
+create table IF NOT EXISTS operation_type
+(
+    operation_type ENUM ('ADD', 'REMOVE', 'UPDATE')
+);
+
+create table IF NOT EXISTS  FEED_LIST
+(
+    EVENT_ID   BIGINT not null,
+    USER_ID    BIGINT not null,
+    EVENT_TYPE  ENUM ('LIKE', 'REVIEW', 'FRIEND'),
+    OPERATION_TYPE ENUM ('ADD', 'REMOVE', 'UPDATE'),
+    ENTITY_ID BIGINT not null,
+    FEED_DATE TIMESTAMP,
+    constraint FEED_LIST_PK
+        primary key (EVENT_ID),
+    constraint FEED_LIST_FK
+        foreign key (USER_ID) references USERS
+            on update cascade on delete cascade
+);
+
+create unique index IF NOT EXISTS FEED_LIST_ID_UINDEX
+    on FEED_LIST (EVENT_ID);
+
+create table IF NOT EXISTS  DIRECTOR
+(
+    ID   BIGINT not null,
+    NAME CHARACTER VARYING(250),
+    constraint DIRECTOR_PK
+        primary key (ID)
+);
+
+create unique index IF NOT EXISTS DIRECTOR_ID_UINDEX
+    on DIRECTOR (ID);
+
+create table IF NOT EXISTS FILM_DIRECTOR
+(
+    FILM_ID BIGINT not null,
+    DIRECTOR_ID BIGINT not null,
+    constraint FILM_DIRECTOR_PK
+        primary key (FILM_ID, DIRECTOR_ID),
+    constraint FILM_DIRECTOR_FILM_FK
+        foreign key (FILM_ID) references FILM
+            on update cascade on delete cascade,
+    constraint FILM_DIRECTOR_FK
+        foreign key (DIRECTOR_ID) references DIRECTOR
+            on update cascade on delete cascade
+);
+
+create unique index IF NOT EXISTS FILM_DIRECTOR_ID_UINDEX
+    on FILM_DIRECTOR (FILM_ID, DIRECTOR_ID);
