59608ddc9d069da787accdda2cdf27e271614c20
DusynHub <gooddayemail@gmail.com>
2023-03-26 18:05:06 +0300

    fix: Исправление checkstyle для класса Reviews

--- src/main/java/ru/yandex/practicum/javafilmorate/model/Review.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/model/Review.java
@@ -1,9 +1,7 @@
 package ru.yandex.practicum.javafilmorate.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
